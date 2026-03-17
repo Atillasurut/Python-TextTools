199e1617c90d4501f508cfbeae904e68ae2b5311
Atilla Sürüt <Atillasrood21@gmail.com>
2026-03-17 17:42:23 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Python-TextTools
+Python Text Tools
+
+A collection of simple text manipulation tools written in Python.
+
+Features
+
+Reverse text
+
+Palindrome checker
+
+Word counter
+
+Character counter
+
+How to Run
+python text_tools.py
+Requirements
+
+Python 3.x
---
+++ text_tools.py
@@ -0,0 +1,76 @@
+class  Utils:
+    def __init__(self,numbers=None, text="" , password=""):
+        self.numbers = numbers
+        self.text = text
+        self.password =password
+    
+    def add(self,a,b):
+        return a+b
+    
+    def subtract(self,a,b):
+        return a-b
+    
+    def multiply(self,a,b):
+        return a*b
+    
+    def divide(self,a,b):
+        if b ==  0:
+            return "Undefined"
+        else:
+            return a/b
+        
+    def is_even(self,num):
+        return num % 2 ==0
+    
+    def  is_odd(self,num):
+        return num % 2 != 0
+    
+    def biggest(self):
+        if not self.numbers:
+            return None
+        big =self.numbers[0]
+        for n in self.numbers:
+            if n > big:
+                big =n
+        return big
+    
+    def smallest(self):
+       if not self.numbers:
+           return None
+       small =self.numbers[0]
+       for n in self.numbers:
+           if n < small:
+               small = n 
+       return small
+    
+    def reverse_text(self):
+        return self.text[::-1]
+    
+    def is_palindrome(self):
+        return self.text == self.text[::-1]
+    
+    def check_password(self):
+       if len(self.password) >= 8:
+           return "Strong"
+       else:
+           return "Weak"
+utils = Utils(numbers=[6,7,8,3,10,15], text="madam", password="Sayat3625")
+
+print(utils.add(5,3))
+print(utils.subtract(10,5))
+print(utils.multiply(4,7))
+print(utils.divide(20,4))
+print(utils.divide(20,0))
+print(utils.is_even(6))
+print(utils.is_even(7))
+print(utils.is_odd(5))
+print(utils.is_odd(8))
+print(utils.biggest())
+print(utils.smallest())
+print(utils.reverse_text())
+print(utils.is_palindrome())
+print(utils.check_password())
+
+
+         
+            
